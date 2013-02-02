0d484a5f01e8ed28c36780cda04b27721c210fb5
Kenichi Kamiya <kachick1@gmail.com>
2013-02-03 01:15:26 +0900

    Simplified to get singleton_class
    * 1.9.2+ supports Kernel#singleton_class

--- lib/method_repository.rb
+++ lib/method_repository.rb
@@ -11,8 +11,7 @@ module MethodRepository
       klass_name = base.class.to_s == 'Class' ? base.to_s : base.class.to_s
 
       if (methods = @targets[klass_name])
-        singleton_class = class << base; self; end
-        singleton_class.class_eval do
+        base.singleton_class.class_eval do
           methods.each do |method|
             define_method method[:name], method[:block]
           end
