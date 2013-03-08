e781c8e998cf834f02b69d8a16f0dc46c89e78a7
Kentaro Kuribayashi <kentarok@gmail.com>
2013-03-08 10:55:03 +0900

    Added extendable_by and includable_by when users want to designate classes.

--- lib/method_repository.rb
+++ lib/method_repository.rb
@@ -2,13 +2,21 @@ require_relative "method_repository/version"
 
 module MethodRepository
   def self.included(base)
-    base.instance_variable_set(:@targets, {})
+    base.instance_variable_set(:@targets,    {})
+    base.instance_variable_set(:@extendable, [])
+    base.instance_variable_set(:@includable, [])
     base.extend(ModuleMethods)
   end
+
+  class NotPermittedError < RuntimeError; end
 
   module ModuleMethods
     def extended(base)
       klass_name = base.class.to_s == 'Class' ? base.to_s : base.class.to_s
+
+      if !@extendable.empty? && !@extendable.include?(klass_name)
+        raise MethodRepository::NotPermittedError.new("Extending #{klass_name} by this module is not permitted")
+      end
 
       if (methods = @targets[klass_name])
         base.singleton_class.class_eval do
@@ -20,13 +28,27 @@ module MethodRepository
     end
 
     def included(base)
-      if (methods = @targets[base.to_s])
+      klass_name = base.to_s
+
+      if !@includable.empty? && !@includable.include?(klass_name)
+        raise MethodRepository::NotPermittedError.new("Including #{klass_name} by this module is not permitted")
+      end
+
+      if (methods = @targets[klass_name])
         base.class_eval do
           methods.each do |method|
             define_method method[:name], method[:block]
           end
         end
       end
+    end
+
+    def extendable_by(*klasses)
+      @extendable.push(*klasses)
+    end
+
+    def includable_by(*klasses)
+      @includable.push(*klasses)
     end
 
     def insert(name, klasses = {}, &block)
--- spec/lib/method_repository_spec.rb
+++ spec/lib/method_repository_spec.rb
@@ -1,6 +1,18 @@
 require_relative '../spec_helper'
 
 describe MethodRepository do
+  module Extendable
+    include MethodRepository
+
+    extendable_by 'Hoge', 'Piyo'
+  end
+
+  module Includable
+    include MethodRepository
+
+    includable_by 'Hoge', 'Piyo'
+  end
+
   module Repository
     include MethodRepository
 
@@ -8,10 +20,58 @@ describe MethodRepository do
     insert :method2, in: %w[Baz]     do; end
   end
 
+  class Hoge; end
+  class Fuga; end
+  class Piyo; end
+
   class Foo; end
   class Bar; end
   class Baz; end
   class Qux; end
+
+  describe '.extendable_by' do
+    context 'when classes are permitted as extendable' do
+      before {
+        Hoge.extend(Extendable)
+        Piyo.extend(Extendable)
+      }
+
+      it {
+        expect(Hoge).to be_a_kind_of Extendable
+        expect(Piyo).to be_a_kind_of Extendable
+      }
+    end
+
+    context 'when classes are not permitted as extendable' do
+      it {
+        expect {
+          Fuga.extend(Extendable)
+        }.to raise_error(MethodRepository::NotPermittedError)
+      }
+    end
+  end
+
+  describe '.includable_by' do
+    context 'when classes are permitted as includable' do
+      before {
+        Hoge.send(:include, Includable)
+        Piyo.send(:include, Includable)
+      }
+
+      it {
+        expect(Hoge.new).to be_a_kind_of Includable
+        expect(Piyo.new).to be_a_kind_of Includable
+      }
+    end
+
+    context 'when classes are not permitted as includable' do
+      it {
+        expect {
+          Fuga.send(:include, Includable)
+        }.to raise_error(MethodRepository::NotPermittedError)
+      }
+    end
+  end
 
   describe '.insert' do
     it {
@@ -94,5 +154,9 @@ describe MethodRepository do
       expect(Baz.new.respond_to?(:method2)).to be true
       expect(Qux.new.respond_to?(:method2)).to be false
     }
+  end
+
+  describe '.includable_by' do
+    context 'when classes'
   end
 end
